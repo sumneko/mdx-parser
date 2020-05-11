826ea11f10be89839b01940d43f77edd1b0aae0a
最萌小汐 <sumneko@hotmail.com>
2020-05-11 16:22:32 +0800

    mdl 的编码解码

--- mdxparser/mdl.lua
+++ mdxparser/mdl.lua
@@ -1,20 +1,30 @@
 local l = require 'lpeglabel'
 
-local tonumber  = tonumber
-local type      = type
-local assert    = assert
-local tostring  = tostring
-local error     = error
-local tableSort = table.sort
-local ipairs    = ipairs
-local osClock   = os.clock
-local print     = print
-local select    = select
+local tonumber     = tonumber
+local type         = type
+local assert       = assert
+local tostring     = tostring
+local error        = error
+local tableSort    = table.sort
+local ipairs       = ipairs
+local osClock      = os.clock
+local print        = print
+local select       = select
+local tableConcat  = table.concat
+local setmetatable = setmetatable
 
 _ENV = nil
 
-local function State(key, ...)
-    local state = { key = key }
+local Tab = setmetatable({}, { __index = function (self, n)
+    self[n] = ('\t'):rep(n)
+    return self[n]
+end })
+
+local function State(static, key, ...)
+    local state = {
+        key    = key,
+        static = static,
+    }
     local num = select('#', ...)
     if num >= 1 then
         state.value = select(num, ...)
@@ -33,28 +43,94 @@ local mdlParser = l.P {
     l.Ct((l.V'Token' + #l.P(1) * l.T'Unknown')^0),
     Token   = l.V'Space' + l.V'State' + l.V'Value',
     Space   = l.S' \t\r\n'^1 + l.V'Comment',
-    Empty   = l.V'Space'^-1,
-    State   = l.V'Word' * l.V'Empty' * ((l.V'String' + l.V'Number') * l.V'Empty')^0 * l.V'Value'^-1 / State,
-    Value   = l.V'Word' + l.V'String' + l.V'Table' + l.V'NumPair' + l.V'Number',
-    Seg     = l.P',',
+    Empty   = l.V'Space'^0,
+    State   = (l.C(l.P'static') + l.Cc(nil)) * l.V'Empty' * l.V'Word' * l.V'Empty' * ((l.V'String' + l.V'Number') * l.V'Empty')^0 * l.V'Value'^-1 / State,
+    Value   = l.V'Word' + l.V'String' + l.V'Array' + l.V'Struct' + l.V'NumPair' + l.V'Number',
+    Seg     = l.V'Empty' * l.P',' * l.V'Empty',
     Comment = l.P'//' * (1 - l.S'\r\n')^0,
-    Number  = l.C(l.P'-'^-1 * l.R'09'^1 * l.V'Decimal'^-1) / tonumber,
-    NumPair = l.Ct(l.V'Number' * l.V'Space'^-1 * l.P':' * l.V'Space'^-1 * (l.V'Number' + l.V'Table')),
+    Number  = l.C(l.P'-'^-1 * l.R'09'^1 * l.V'Decimal'^-1),
+    NumPair = l.Ct(l.Cc'NumPair' * l.V'Number' * l.V'Space'^-1 * l.P':' * l.V'Space'^-1 * (l.V'Number' + l.V'Array')),
     Decimal = l.P'.' * l.R'09'^0,
-    String  = l.P'"' * l.C((1 - l.P'"')^0) * l.P'"',
+    String  = l.C(l.P'"' * (1 - l.P'"')^0 * l.P'"'),
     Char    = l.R('az', 'AZ', '09', '__'),
     Word    = l.C(l.R('az', 'AZ', '__') * l.R('az', 'AZ', '09', '__')^0),
-    Table   = l.Ct(l.P'{' * (l.V'Token' * l.V'Seg'^-1 + (l.T'Unknown' - l.P'}'))^0 * l.P'}'),
+    Afield  = l.V'Empty' * l.V'Value' * l.V'Empty',
+    Array   = l.Ct(l.Cc'Array' * l.P'{'
+                * (l.V'Afield' * l.V'Seg')^0 * l.V'Afield'^-1
+            * l.V'Empty' * l.P'}'),
+    Sfield  = l.V'Empty' * l.V'State' * l.V'Empty',
+    Struct  = l.Ct(l.Cc'Struct' * l.P'{'
+                * (l.V'Sfield' * l.V'Seg'^-1 + #(1 - l.P'}') * l.T'Unknown')^0
+            * l.V'Empty' * l.P'}'),
 }
 
+local function encodeAttribute(buf, attributes)
+    for i = 1, #attributes do
+        buf[#buf+1] = ' '
+        buf[#buf+1] = attributes[i]
+    end
+end
+
+local encodeValue
+local function encodeState(buf, state, tab)
+    buf[#buf+1] = Tab[tab]
+    buf[#buf+1] = state.key
+    if state.attribute then
+        encodeAttribute(buf, state.attribute)
+    end
+    if state.value then
+        encodeValue(buf, state.value, tab)
+    end
+end
+
+local function encodeTable(buf, value, tab)
+    local mode = value[1]
+    if mode == 'Struct' then
+        buf[#buf+1] = '{\r\n'
+        for i = 2, #value do
+            encodeState(buf, value[i], tab + 1)
+            buf[#buf+1] = ',\r\n'
+        end
+        buf[#buf+1] = Tab[tab]
+        buf[#buf+1] = '}'
+    elseif mode == 'Array' then
+        buf[#buf+1] = '{'
+        for i = 2, #value do
+            encodeValue(buf, value[i], tab)
+            if i < #value then
+                buf[#buf+1] = ','
+            end
+        end
+        buf[#buf+1] = ' }'
+    elseif mode == 'NumPair' then
+        buf[#buf+1] = value[2]
+        buf[#buf+1] = ': '
+        encodeValue(buf, value[3], tab)
+    end
+end
+
+function encodeValue(buf, value, tab)
+    buf[#buf+1] = ' '
+    if type(value) == 'table' then
+        encodeTable(buf, value, tab)
+    else
+        buf[#buf+1] = value
+    end
+end
+
 local function encode(model)
+    local buf = {}
+    for _, chunk in ipairs(model) do
+        encodeState(buf, chunk, 0)
+        buf[#buf+1] = '\r\n'
+    end
+    return tableConcat(buf)
 end
 
 local function decode(buf)
-    local clock = osClock()
-    local tokens, err, pos = mdlParser:match(buf)
-    assert(tokens, 'Parse mdl failed at:' .. tostring(pos))
-    print(osClock() - clock)
+    local model, err, pos = mdlParser:match(buf)
+    assert(model, 'Parse mdl failed at:' .. tostring(pos))
+    return model
 end
 
 return {
--- test/main.lua
+++ test/main.lua
@@ -5,13 +5,17 @@ local parser = require 'mdxparser'
 local root   = fs.current_path()
 local mdxDir = root / 'test' / 'mdx'
 local mdlDir = root / 'test' / 'mdl'
+local tempDir = root / 'temp'
 
 fs.create_directories(mdlDir)
+fs.create_directories(tempDir)
 for path in fsu.scan(mdxDir) do
     --local buf1 = fsu.loadFile(mdxDir / path)
     local buf2 = fsu.loadFile(mdlDir / (path:stem() .. '.mdl'))
     --local model1 = parser.mdx.decode(buf1)
     local model2 = parser.mdl.decode(buf2)
     --assert(util.equal(model1, model2))
+    local mdlBuf = parser.mdl.encode(model2)
+    fsu.saveFile(tempDir / (path:stem() .. '.mdl'), mdlBuf)
 end
 print(1)
